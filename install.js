695d0a4b2f1d20b8a2ceddd4629d53bb88c40dbb
AdolfGitler1945 <yt272006@gmail.com>
2025-09-25 01:56:09 +0530

    Delete install.js

--- install.js
+++
@@ -1,9 +0,0 @@
-// Installation script
-const install = () => {
-    console.log('Installing dependencies...');
-    console.log('npm install express bcryptjs jsonwebtoken cors');
-    console.log('npm install @google/generative-ai');
-    console.log('Installation complete!');
-};
-
-install();
