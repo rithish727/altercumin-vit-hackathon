9d3ed7d841586a6aef92bb0dcb4c484e9ff2d915
AdolfGitler1945 <yt272006@gmail.com>
2025-09-25 01:57:44 +0530

    Delete sound-fix.js

--- sound-fix.js
+++
@@ -1,9 +0,0 @@
-// Fix for sound issues
-const fixSounds = () => {
-    console.log('Fixing sound issues...');
-    console.log('Updating sound URLs...');
-    console.log('Adding NCS tracks...');
-    console.log('Sound fix complete!');
-};
-
-fixSounds();
